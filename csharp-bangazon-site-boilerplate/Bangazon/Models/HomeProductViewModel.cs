6586fe4004f50e5a08584dabacf3214c136af6fc
AmandaM04 <wreedamitchell@gmail.com>
2018-10-16 11:55:36 -0500

    wip

---
+++ csharp-bangazon-site-boilerplate/Bangazon/Models/HomeProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.Models
+{
+    public class HomeProductViewModel
+    {
+        public int ProductId { get; set; }
+
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
