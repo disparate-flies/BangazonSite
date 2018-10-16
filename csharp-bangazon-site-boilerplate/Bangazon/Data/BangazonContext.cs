5fe9ad80841d6eae968ca9fc2faddb9ad1bb5def
Leah <leahgwin@gmail.com>
2018-10-16 11:17:08 -0500

    added product type controller

---
+++ csharp-bangazon-site-boilerplate/Bangazon/Data/BangazonContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bangazon.Models
+{
+    public class BangazonContext : DbContext
+    {
+        public BangazonContext (DbContextOptions<BangazonContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bangazon.Models.ProductType> ProductType { get; set; }
+    }
+}
